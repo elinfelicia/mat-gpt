26c599183f8a140a26d392b787683256b5ae2017
elinfelicia <elinriihimaki@gmail.com>
2024-05-31 10:32:41 +0200

    login in and sign up styling

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,48 +1,49 @@
-import "../styles/LoginPage.css"
-import {Link} from "react-router-dom";
-import {useForm} from "react-hook-form"
-import { DevTool } from "@hookform/devtools"
-
-
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 function LoginPage() {
-    const form= useForm();
-    const {register,control,handleSubmit,formState} = form;
-    const {errors}= formState;
+    const form = useForm();
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
     const onSubmit = (data) => {
-        console.log('Form submitted',data)
-    }
+        console.log('Form submitted', data);
+    };
 
     return (
-        <div className="containerLogin flex justify-center p-5">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 h-fit">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Login to your account
-                    </h1>
-                    <form onSubmit={handleSubmit(onSubmit)} noValidate className="space-y-4 md:space-y-6">
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" placeholder="name@email.com" 
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            {...register("email",{required:'Email is required'})}/>
-                            <p className="error">{errors.email?.message}</p>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" 
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                            {...register("password",{required:'Password is required'})}/>
-                             <p className="error">{errors.password?.message}</p>
-                        </div>
-                        <button type="submit" className="w-full text-white bg-emerald-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <Link className="font-medium text-blue-600 hover:underline dark:text-blue-500" to="/signup"> Sign up </Link>
-                        </p>
-                    </form>
-                    <DevTool control={control}/>
+        <div className="containerLogin flex justify-center p-10">
+            <form onSubmit={handleSubmit(onSubmit)} noValidate className="w-full max-w-lg">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-10">
+                    Login to your account
+                </h1>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full px-3">
+                        <label htmlFor="email" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Your email
+                        </label>
+                        <input type="email" name="email" id="email" placeholder="name@email.com"
+                               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                               {...register("email", { required: 'Email is required' })}/>
+                        <p className="error">{errors.email?.message}</p>
+                    </div>
+                    <div className="w-full px-3">
+                        <label htmlFor="password" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Password
+                        </label>
+                        <input type="password" name="password" id="password" placeholder="Password"
+                               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                               {...register("password", { required: 'Password is required' })}/>
+                        <p className="error">{errors.password?.message}</p>
+                    </div>
                 </div>
-            </div>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400 mt-4">
+                    Don’t have an account yet? <Link className="font-medium text-blue-600 hover:underline dark:text-blue-500" to="/signup">Sign up</Link>
+                </p>
+                <button type="submit" className="landingButton1" >
+                    Login
+                </button>
+            </form>
+            <DevTool control={control}/>
         </div>
     );
 }
--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,42 +1,38 @@
-import "../styles/LoginPage.css"
-import {Link} from "react-router-dom";
-import {useForm} from "react-hook-form"
-import { DevTool } from "@hookform/devtools"
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
-function SignUp()
-{
-    const form= useForm();
-    const {register,control,handleSubmit,formState,watch} = form;
-    const {errors}= formState;
+function SignUp() {
+    const form = useForm();
+    const { register, control, handleSubmit, formState, watch } = form;
+    const { errors } = formState;
     const onSubmit = (data) => {
-        console.log('Form submitted',data)
-    }
-    const WatchPassword = watch("password")
+        console.log('Form submitted', data);
+    };
+    const WatchPassword = watch("password");
 
-    return(
+    return (
         <div className="containerLogin flex justify-center p-10">
-            <form onSubmit={handleSubmit(onSubmit)} noValidate className=" w-full max-w-lg ">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-10">
-                        Sign up
-                    </h1>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate className="w-full max-w-lg">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-10">
+                    Sign up
+                </h1>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" 
-                    htmlFor="firstname">
-                        First Name*
-                    </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                    id="firstname" name="firstname" type="text" placeholder="John" {...register("firstname",{required:'Firstname is required'})}/>
-                    <p className="error">{errors.firstname?.message}</p>
-                    
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="firstname">
+                            First Name*
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                               id="firstname" name="firstname" type="text" placeholder="John" {...register("firstname", { required: 'Firstname is required' })}/>
+                        <p className="error">{errors.firstname?.message}</p>
                     </div>
                     <div className="w-full md:w-1/2 px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="lastname">
-                        Last Name*
-                    </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-                    id="lastname" name="lastname" type="text" placeholder="Doe" {...register("lastname",{required:'Lastname is required'})}/>
-                    <p className="error">{errors.lastname?.message}</p>
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="lastname">
+                            Last Name*
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                               id="lastname" name="lastname" type="text" placeholder="Doe" {...register("lastname", { required: 'Lastname is required' })}/>
+                        <p className="error">{errors.lastname?.message}</p>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
@@ -44,46 +40,52 @@ function SignUp()
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">
                             Email*
                         </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-                        id="email" name="email" type="email" placeholder="name@email.com" 
-                        {...register("email",{
-                            required:'Email is required',
-                            pattern:{
-                                value:/^[a-zA-Z0-9.!#$%&'*+/=`^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                                message:"Invalid email format"}
-                            })}/>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                               id="email" name="email" type="email" placeholder="name@email.com"
+                               {...register("email", {
+                                   required: 'Email is required',
+                                   pattern: {
+                                       value: /^[a-zA-Z0-9.!#$%&'*+/=`^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                       message: "Invalid email format"
+                                   }
+                               })}/>
                         <p className="error">{errors.email?.message}</p>
                     </div>
                     <div className="w-full px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="password">
                             Password*
                         </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-                        id="password" name="password" type="password" placeholder="Password" {...register("password",{required:'Password is required'})}/>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                               id="password" name="password" type="password" placeholder="Password" {...register("password", { required: 'Password is required' })}/>
                         <p className="error">{errors.password?.message}</p>
                     </div>
                     <div className="w-full px-3">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="password2">
                             Confirm Password*
                         </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-                        id="password2" name="password2" type="password" placeholder="Confirm Password" 
-                        {...register("password2",{required:'Password confirmation is needed',validate:(fieldValue)=>{
-                            return (fieldValue == WatchPassword || "Passwords must be same")
-                        }       
-                        })}/>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                               id="password2" name="password2" type="password" placeholder="Confirm Password"
+                               {...register("password2", {
+                                   required: 'Password confirmation is needed',
+                                   validate: (fieldValue) => {
+                                       return (fieldValue === WatchPassword || "Passwords must be same")
+                                   }
+                               })}/>
                         <p className="error">{errors.password2?.message}</p>
                     </div>
                 </div>
-                <div className="flex flex-wrap -mx-3 mb-2">
-                    <button className="landingButton1" >Sign up</button>
-                    <Link className="landingButton2" to="/login"> Login </Link>
+                <div className="flex flex-col items-center space-y-4 w-full">
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
+                        Already have an account? <Link className="font-medium text-blue-600 hover:underline dark:text-blue-500" to="/login">Log in</Link>
+                    </p>
+                    <button className="landingButton1">
+                        Sign up
+                    </button>
                 </div>
             </form>
             <DevTool control={control}/>
         </div>
-        
-    )
+    );
 }
 
-export default SignUp
+export default SignUp;
