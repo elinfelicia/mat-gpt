4d16c04eb68c969565a8f8be8461044f630f9566
sophieAdlen <144331073+sophieAdlen@users.noreply.github.com>
2024-05-31 14:35:10 +0200

    background image landing page

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -4,17 +4,9 @@ import {Link} from "react-router-dom";
 
 
 function LandingPage() {
-  const pageStyle = {
-    background: "url('/images/bg-desktop.png') center/cover",
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh', // Set the height if necessary
-    width: '100vw' // Set the width if necessary
-  };
 
   return (
-    <div style={pageStyle}>
+    <div className="pageStyle">
       {/* Content */}
       <div className="container">
             <img className="heading" src="/images/chefbotLogo.png" alt="" />
